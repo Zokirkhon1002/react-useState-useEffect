45a09b49954f09d7efe5616643f20a652b95e841
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2021-08-30 04:41:19 +0900

    functional components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Clicker from "./components/Clicker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [ isClicker, setClicker ] = useState(true);
+
+  useEffect(() => {
+    console.log('DidMount', isClicker);
+
+    return ()=> console.log('See you again')
+  }, [isClicker]);
+
+ return(
+   <div>
+     <h1>React App</h1>
+     <button className="blue" onClick={() => setClicker(!isClicker)}>Toggle Clicker</button>
+      {isClicker && <Clicker />}
+   </div>
+ )
 }
-
-export default App;
---
+++ src/components/Clicker.jsx
@@ -0,0 +1,26 @@
+import React, { useState} from "react";
+
+
+const Clicker = () => {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+      setCount((prevState) => {
+       return prevState + 1;
+      });
+      setCount(prev => prev+1)
+    }
+  
+    return (
+      <div className="App app2">
+        <h1>Counter: {count}</h1>
+        <button onClick={increment} className="info">
+          Increment +{" "}
+        </button>
+        <button onClick={() => setCount(count - 1)} className="danger">
+          Decremenet -{" "}
+        </button>
+      </div>
+    );
+}
+export default Clicker;
